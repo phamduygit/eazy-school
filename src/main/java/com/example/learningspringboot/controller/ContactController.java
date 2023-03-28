76e4fa8fecb4f4e64201cddde9c916354cfc1faf
phamduygit <minhduy.bthuy@gmail.com>
2023-03-28 23:47:06 +0700

    complete section 11

--- src/main/java/com/example/learningspringboot/controller/ContactController.java
+++ src/main/java/com/example/learningspringboot/controller/ContactController.java
@@ -2,14 +2,19 @@ package com.example.learningspringboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.example.learningspringboot.model.Contact;
 import com.example.learningspringboot.service.ContactService;
 
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class ContactController {
     private final ContactService contactService;
 
@@ -19,12 +24,17 @@ public class ContactController {
 
     @RequestMapping(value = "/contact")
     public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
     @RequestMapping(value = "/saveMsg", method = RequestMethod.POST)
-    public ModelAndView saveMessage(Contact contact) {
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+        if (errors.hasErrors()) {
+            log.error("Contact form validation failed due to: " + errors.toString());
+            return "contact.html";
+        }
         contactService.saveMessage(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 }
